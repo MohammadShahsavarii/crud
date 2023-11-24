a36400bc52d6814854876f3d066b61324340d74e
MohammadShahsavarii <shahsavari.mo@gmail.com>
2023-11-24 19:17:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+include 'functions.php';
+
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Welcome to the car dealer!</p>
+	<p>The best is waiting for you here</p>
+</div>
+
+<?=template_footer()?>
